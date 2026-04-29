38fd07988d1566987e4e0f44ae857cad032680a0
Andoni <98283996+Andonigt04@users.noreply.github.com>
2026-04-29 09:17:42 +0200

    Implement HarukaVulkanEngine and IEngine interface for Vulkan integration

--- src/editor_app.h
+++ src/editor_app.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#include "IEngine.h"
+#include <imgui.h>
+#include <SDL3/SDL.h>
+#include <memory>
+
+#include "engine/IEngine.h"
 #include "core/project.h"
 #include "core/scene.h"
 #include "core/camera.h"
@@ -23,9 +27,6 @@
 #include "panels/export_panel.h"
 #include "panels/planet_terrain_editor.h"
 #include "menu_bar.h"
-#include <imgui.h>
-#include <SDL3/SDL.h>
-#include <memory>
 
 class MenuBar;
 
